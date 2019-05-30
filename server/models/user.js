c22ffe1bdfadd2221c11b0e905cc6c1ff22c9685
allebd <delebella@yahoo.com>
2019-05-30 13:33:55 +0100

    remove eslint

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,6 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING
   }, {});
   User.associate = function(models) {
-    // associations can be defined here
   };
   return User;
 };
