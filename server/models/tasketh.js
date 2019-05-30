019b972abe73073284eaca899a29bbebe5fa7d37
allebd <delebella@yahoo.com>
2019-05-30 14:33:22 +0100

    eslint update

--- server/models/tasketh.js
+++ server/models/tasketh.js
@@ -1,10 +1,10 @@
-'use strict';
+// eslint-disable
 module.exports = (sequelize, DataTypes) => {
   const tasketh = sequelize.define('tasketh', {
     title: DataTypes.STRING,
-    iscompleted: DataTypes.BOOLEAN
+    iscompleted: DataTypes.BOOLEAN,
   }, {});
-  tasketh.associate = function(models) {
+  tasketh.associate = function (models) {
     // associations can be defined here
   };
   return tasketh;
